c65d71d895259653a3cf5defe07a9dc08b16b471
Kiran Shila <me@kiranshila.com>
2019-12-01 11:29:19 -0500

    Little cleaning

--- 1.jl
+++ 1.jl
@@ -2,15 +2,11 @@ function fuelRequirement1(mass)
     return floor(mass / 3) - 2
 end
 
-@show result = sum([fuelRequirement1(parse(Float64,i)) for i in readlines("input1.txt")])
+@show sum([fuelRequirement1(parse(Float64,i)) for i in readlines("input1.txt")])
 
 function fuelRequirement2(mass)
     requirement = floor(mass / 3) - 2
-    if requirement <= 0
-        return 0
-    else
-        return requirement + fuelRequirement(requirement)
-    end
+    requirement <= 0 ? 0 : requirement + fuelRequirement(requirement)
 end
 
-@show result = sum([fuelRequirement2(parse(Float64,i)) for i in readlines("input1.txt")])
+@show sum([fuelRequirement2(parse(Float64,i)) for i in readlines("input1.txt")])
