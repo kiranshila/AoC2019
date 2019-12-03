f0e32fd055e6fc5cffd55e7e183f3f5bf50900f1
Kiran Shila <kiranshila@Kirans-MacBook-Pro.local>
2019-12-02 23:17:50 -0500

    Updated for offsetarrays

--- 2.jl
+++ 2.jl
@@ -1,18 +1,20 @@
+using OffsetArrays
+
 # Part 1
-program = [parse(Int64,i) for i in split(readline("2.txt"),',')]
-program[2] = 12
-program[3] = 2
-function runIntcode!(program,offset=1)
+program = OffsetArray([parse(Int64,i) for i in split(readline("2.txt"),',')],-1)
+program[1] = 12
+program[2] = 2
+function runIntcode!(program,offset=0)
     # Step 1 - Get opcode
     opcode = program[offset]
     if opcode == 1
         # Add routine
-        program[program[offset+3]+1] = program[program[offset+1]+1] + program[program[offset+2]+1]
+        program[program[offset+3]] = program[program[offset+1]] + program[program[offset+2]]
     elseif opcode == 2
         # Multiply routine
-        program[program[offset+3]+1] = program[program[offset+1]+1] * program[program[offset+2]+1]
+        program[program[offset+3]] = program[program[offset+1]] * program[program[offset+2]]
     elseif opcode == 99
-        return program[1]
+        return program[0]
     end
     runIntcode!(program,offset+4)
 end
@@ -22,9 +24,9 @@ end
 # Part 2
 function matchTarget(target)
     for i in 0:99, j in 0:99
-        program = [parse(Int64,i) for i in split(readline("2.txt"),',')]
-        program[2] = i
-        program[3] = j
+        program = OffsetArray([parse(Int64,i) for i in split(readline("2.txt"),',')],-1)
+        program[1] = i
+        program[2] = j
         if runIntcode!(program) == target
             println("Got $(100 * i + j)")
         end
